67d379241369eb16e48d0ba8bc4ae97a04d943b6
erikvw <ew2789@gmail.com>
2016-05-30 08:29:40 +0200

    remove ref to basemodeladmin

--- edc_configuration/admin.py
+++ edc_configuration/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
-from edc_base.modeladmin.admin import BaseModelAdmin
 
 from .models import GlobalConfiguration
 
 
-class GlobalConfigurationAdmin(BaseModelAdmin):
+class GlobalConfigurationAdmin(admin.ModelAdmin):
     list_display = ('category', 'attribute', 'value', 'convert')
     list_filter = ('category', )
     search_fields = ('attribute', 'category', 'attribute', 'value')
